691ead1f9b9edd93732e3dc9ef161f08e3eabece
benjaminhuanghuang <bhuang@rms.com>
2022-07-13 23:37:01 -0700

    4 test qemu exit

--- writing-an-os-in-rust/04-testing/blog_os/src/main.rs
+++ writing-an-os-in-rust/04-testing/blog_os/src/main.rs
@@ -2,7 +2,7 @@
 #![no_main]
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
-// custom test frame create test_main()
+// custom test FWK create test_main()
 #![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
@@ -32,9 +32,28 @@ fn test_runner(tests: &[&dyn Fn()]) {
   for test in tests {
     test();
   }
+  /// new
+  exit_qemu(QemuExitCode::Success);
 }
 
 #[test_case]
 fn trivial_assertion() {
   assert_eq!(1, 1);
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+  Success = 0x10,
+  Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+  use x86_64::instructions::port::Port;
+
+  unsafe {
+    // 写端口 0xf4, I/O size 为 4(u32)
+    let mut port = Port::new(0xf4);
+    port.write(exit_code as u32);
+  }
 }
