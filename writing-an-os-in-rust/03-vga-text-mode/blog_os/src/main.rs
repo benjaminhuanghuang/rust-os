c07b2f8b519a8d086b320452347beea8c40c0f5b
benjaminhuanghuang <bhuang@rms.com>
2022-07-13 18:06:44 -0700

    3 print

--- writing-an-os-in-rust/03-vga-text-mode/blog_os/src/main.rs
+++ writing-an-os-in-rust/03-vga-text-mode/blog_os/src/main.rs
@@ -8,22 +8,15 @@ use core::panic::PanicInfo;
 
 #[no_mangle] // 不重整函数名
 pub extern "C" fn _start() -> ! {
-  use core::fmt::Write;
-  vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-  write!(
-    vga_buffer::WRITER.lock(),
-    ", some numbers: {} {}",
-    42,
-    1.337
-  )
-  .unwrap();
+  println!("Hello world {}", "OS");
 
+  panic!("Some panic message");
   loop {}
 }
 
 /// 这个函数将在panic时被调用
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
   println!("{}", info);
   loop {}
 }
