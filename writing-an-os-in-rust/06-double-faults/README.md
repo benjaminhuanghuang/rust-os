f861155c28299a46d06ac9c660f3578f712c4eaa
benjaminhuanghuang <bhuang@rms.com>
2022-07-14 23:28:43 -0700

    6 start

---
+++ writing-an-os-in-rust/06-double-faults/README.md
@@ -0,0 +1,3 @@
+# Double Faults
+
+https://os.phil-opp.com/double-fault-exceptions/
---
+++ writing-an-os-in-rust/06-double-faults/blog_os/src/main.rs
@@ -0,0 +1,39 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(blog_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use blog_os::println;
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+  println!("Hello World{}", "!");
+
+  blog_os::init();
+
+  // invoke a breakpoint exception
+  x86_64::instructions::interrupts::int3(); // new
+
+  #[cfg(test)]
+  test_main();
+
+  // 表明异常被catch住, 程序没有崩溃
+  println!("It did not crash!");
+  loop {}
+}
+
+/// This function is called on panic.
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+  println!("{}", info);
+  loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+  blog_os::test_panic_handler(info)
+}
